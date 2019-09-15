21dac4d122f58c015ce226571a701444a74b1e23
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2019-09-15 11:20:40 -0400

    Updated Scraper, first run of CSV
    Fixed the issues with appending the ID and Body. Still need to fix Date Time. First run of script was successful, CSV the top 50 posts of r/cscareerquestions.

--- Career Scraper.py
+++ Career Scraper.py
@@ -1,4 +1,4 @@
-#! python3
+#!/usr/bin/python
 import praw # Python Reddit API Wrapper
 import pandas as pd
 import datetime as dt
@@ -7,11 +7,11 @@ def get_date(created):
 	return dt.datetime.fromtimestamp(created)
 
 
-reddit = praw.Reddit(client_id='CLIENT_ID', \
-					 client_secret = 'SECRET_CODE', \
+reddit = praw.Reddit(client_id='sUXh7f-hkzflvw', \
+					 client_secret = 'XXX-XXXXXXXXXXX-XXX-XXXXXX', \
 					 user_agent = 'Scrape', \
-					 username = 'REDDIT USERNAME', \
-					 password = 'REDDIT_PASSWORD')
+					 username = 'konic899', \
+					 password = 'PASSWORD')
 
 subreddit = reddit.subreddit('cscareerquestions')
 
@@ -38,11 +38,11 @@ topics_dict = { "title": [], \
 for thread in subreddit.top():
 	topics_dict["title"].append(thread.title)
 	topics_dict["score"].append(thread.score)
-	topics_dict["id"].apppend(thread.id)
+	topics_dict["id"].append(thread.id)
 	topics_dict["url"].append(thread.url)
 	topics_dict["comms_num"].append(thread.num_comments)
 	topics_dict["created"].append(thread.created)
-	topics_dict["body"].append(thread.body)
+	topics_dict["body"].append(thread.selftext)
 
 topics_data = pd.DataFrame(topics_dict)
 
